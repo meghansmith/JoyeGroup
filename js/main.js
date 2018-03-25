4a4b9d74eb4aa6473c867af8e3ef2159b13da01c
Meghan Smith <meghansmithsc@gmail.com>
2018-03-25 13:32:56 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$(".menuHide").addClass('hide');
+	$('#menuButton').click(function(){
+		$(".menuHide").slideToggle('fast');
+	});
+
+	var container = document.querySelector('#container');
+	var msnry = new Masonry( container, {
+	itemSelector: '.eventS'
+	});
+});
